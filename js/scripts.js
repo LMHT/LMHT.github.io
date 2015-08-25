7e9db47735dd00453a7996d38c967382ad4bbd29
Kevin Brian Fahy <kevinbrianfahy@gmail.com>
2015-08-24 21:48:03 -0500

    merged Rafa's files

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,6 @@
-/*$('h1').on('mouseenter', function() {
-  $(this).hide();
-});
-*/
-
-/*$('.tags').on('click', function() {
-  var content = $(this).attr('for');
-  $('#' + content).fadeToggle();
-});
-*/
-/*
-$('.tags').on('click', function() {
-  var content = $(this).attr('for');
-  $('#' + content).show().siblings().hide();
-});
-*/
-
-/*
-$('#menuwidget').menu();
-*/
-
-
 $('#tags').selectmenu({
   select: function() {
   var content = $(this).val();
-  $('#' + content).fadeIn('easing').siblings().hide();
+  $('#' + content).fadeIn(1000).siblings().hide();
   }
 });
