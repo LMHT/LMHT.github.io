385fc98a2051375286c7aa76208495fb4aacdf64
ericanaughton <erica@adorable.io>
2015-12-11 10:59:19 -0600

    Removes comments from js files

--- js/combobox.js
+++ js/combobox.js
@@ -8,11 +8,7 @@ $(function() {
          this.element.hide();
          this._createAutocomplete();
          this._createShowAllButton();
-// placeholder
          this.input.attr("placeholder", this.element.attr('placeholder'));
-         // clear the default selected option so that we see the placeholder text
-        //  this.input.value = '';
-        //  console.log(this.input)
        },
 
        _createAutocomplete: function() {
