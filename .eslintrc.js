0fb9f518c75da28b6f226edcf8d457f965eb968f
ericanaughton <erica@adorable.io>
2016-01-05 10:10:26 -0600

    Changes indentation to spaces, adds modules rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true
-    },
-    "extends": "eslint:recommended"
-    "rules": {
-        "indent": [
-            2,
-            2
-        ],
-        "quotes": [
-            2,
-            "double"
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-        "semi": [
-            2,
-            "always"
-        ]
-    },
+  "ecmaFeatures": {
+    modules: true
+  }
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  "extends": "eslint:recommended"
+  "rules": {
+    "indent": [
+      2,
+      2
+    ],
+    "quotes": [
+      2,
+      "double"
+    ],
+    "linebreak-style": [
+      2,
+      "unix"
+    ],
+    "semi": [
+      2,
+      "always"
+    ]
+  },
 };
