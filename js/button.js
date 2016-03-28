f514cb1049910a0e1b073445354ff3507df5ac5a
Erica Naughton <erica.naughton@gmail.com>
2016-03-28 11:38:40 -0500

    Fixes audio playback

--- js/button.js
+++ js/button.js
@@ -1,37 +1,40 @@
-var $previousButton;
-var audio = new Audio();
-var paused = true;
+$(document).ready(function() {
 
-var setPlaying = function ($button) {
-  $button.addClass('pause');
-  audio.play();
-  paused = false;
-};
+  var $previousButton;
+  var audio = new Audio();
+  var paused = true;
 
-var setPause = function ($button) {
-  $button.removeClass('pause');
-  audio.pause();
-  paused = true;
-};
+  var setPlaying = function ($button) {
+    $button.addClass('pause');
+    audio.play();
+    paused = false;
+  };
 
-audio.onpause = setPause;
+  var setPause = function ($button) {
+    $button.removeClass('pause');
+    audio.pause();
+    paused = true;
+  };
 
-$('body').on( 'click', '.play-button', function(event) {
-  var $button = $(event.target);
-  var audioSrc = $button.data('audio-src');
-  var isPreviousButton = new RegExp(audioSrc).test(audio.src);
+  audio.onpause = setPause;
 
-  if (!isPreviousButton && !paused) {
-    setPause($previousButton);
-  }
+  $('body').on( 'click', '.play-button', function(event) {
+    var $button = $(event.target);
+    var audioSrc = $button.data('audio-src');
+    var isPreviousButton = new RegExp(audioSrc).test(audio.src);
 
-  audio.src = audioSrc;
+    if (!isPreviousButton && !paused) {
+      setPause($previousButton);
+    }
 
-  if (paused) {
-    setPlaying($button);
-  } else {
-    setPause($button);
-  }
+    audio.src = audioSrc;
 
-  $previousButton = $button;
+    if (paused) {
+      setPlaying($button);
+    } else {
+      setPause($button);
+    }
+
+    $previousButton = $button;
+  });
 });
